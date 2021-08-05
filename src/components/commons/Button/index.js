58b9af67cb1ad3872ddc9e1827802076cfe15aa6
Giselli Hara <gi@hara.ninja>
2021-08-05 18:55:29 -0300

    aula 07

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 import get from 'lodash/get';
 import { TextStyleVariants } from '../../foundation/Text';
 import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
+import { propToStyle } from '../../../theme/utils/propToStyle';
 
 const ButtonGhost = css`
   color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
@@ -32,10 +33,12 @@ export const Button = styled.button`
     `,
 })}
 
+  ${propToStyle('margin')}
+  ${propToStyle('display')}
+
   ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
   &:hover,
   &:focus {
     opacity: .5;
   }
-
- `;
+`;
--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import get from 'lodash/get';
 import PropTypes from 'prop-types';
+import { propToStyle } from '../../../theme/utils/propToStyle';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
 const paragraph1 = css`
   ${({ theme }) => css`
@@ -20,40 +22,57 @@ const smallestException = css`
 `;
 
 export const TextStyleVariants = {
-    smallestException,
-    paragraph1,
+  smallestException,
+  paragraph1,
+  title: css`
+    ${({ theme }) => css`
+      font-size: ${theme.typographyVariants.titleXS.fontSize};
+      font-weight: ${theme.typographyVariants.titleXS.fontWeight};
+      line-height: ${theme.typographyVariants.titleXS.lineHeight};
+    `}
+    ${breakpointsMedia({
+    md: css`
+        ${({ theme }) => css`
+          font-size: ${theme.typographyVariants.title.fontSize};
+          font-weight: ${theme.typographyVariants.title.fontWeight};
+          line-height: ${theme.typographyVariants.title.lineHeight};
+        `}
+      `,
+  })}
+  `,
 };
 
 const TextBase = styled.span`
   ${({ variant }) => TextStyleVariants[variant]}
   color: ${({ theme, color }) => get(theme, `colors.${color}.color`)};
+  ${propToStyle('textAlign')}
 `;
 
 export function Text({
-    variant,
-    children,
-    tag,
-    ...props
+  variant,
+  children,
+  tag,
+  ...props
 }) {
-    return (
-        <TextBase
-            as={tag}
-            variant={variant}
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            {...props}
-        >
-            {children}
-        </TextBase>
-    );
+  return (
+    <TextBase
+      as={tag}
+      variant={variant}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
+      {children}
+    </TextBase>
+  );
 }
 
 Text.defaultProps = {
-    tag: 'span',
-    variant: 'paragraph1',
+  tag: 'span',
+  variant: 'paragraph1',
 };
 
 Text.propTypes = {
-    children: PropTypes.node.isRequired,
-    tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span']),
-    variant: PropTypes.oneOf(['paragraph1', 'smallestException']),
+  children: PropTypes.node.isRequired,
+  tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span']),
+  variant: PropTypes.oneOf(['title', 'paragraph1', 'smallestException']),
 };
