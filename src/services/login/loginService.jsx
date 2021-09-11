d39eaccb8588971fd15b38d63c67a76f3d03c404
Giselli Hara <gi@hara.ninja>
2021-09-10 21:30:06 -0300

    adiciona o login

---
+++ src/services/login/loginService.jsx
@@ -0,0 +1,37 @@
+async function HttpClient(url, { headers, body, ...options }) {
+  return fetch(url, {
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+    ...options,
+  })
+    .then((respostaDoServer) => {
+      if (respostaDoServer.ok) {
+        return respostaDoServer.json();
+      }
+
+      throw new Error('Falha em pegar os dados do servidor :(');
+    });
+}
+
+const loginService = {
+  async login({ username, password }) {
+    return HttpClient('https://instalura-api.omariosouto.vercel.app/api/login', {
+      method: 'POST',
+      body: {
+        username, // 'gi'
+        password, // 'senhasegura'
+      },
+    })
+      .then((respostaConvertida) => {
+        // Salvar o Token
+        // Escrever os testes
+        console.log(respostaConvertida);
+        return respostaConvertida;
+      });
+  },
+};
+
+export default loginService;
